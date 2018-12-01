91c44e22f283abbb8ae903cb772cefc971d5f389
karan <karan@blockwala.io>
2018-12-01 23:53:07 +0530

    db name change

--- apps/explorer/config/dev.exs
+++ apps/explorer/config/dev.exs
@@ -3,8 +3,8 @@ use Mix.Config
 # Configure your database
 config :explorer, Explorer.Repo,
   adapter: Ecto.Adapters.Postgres,
-  database: "explorer_dev",
-  hostname: "localhost",
+  database: "explorer",
+  hostname: "blockchainexplorerdata.cbuqrrbjabbr.us-east-1.rds.amazonaws.com",
   pool_size: 20,
   pool_timeout: 60_000,
   timeout: 80_000
