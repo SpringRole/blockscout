ddb0279e18a9089d4972f6cae734c9b36c1a803a
karan <karan@blockwala.io>
2018-12-04 11:56:37 +0530

    local

--- apps/explorer/config/dev.secret.exs
+++ apps/explorer/config/dev.secret.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :explorer, Explorer.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "explorer_dev",
-  hostname: "blockchainexplorerdata.cbuqrrbjabbr.us-east-1.rds.amazonaws.com",
+  hostname: "localhost",
   loggers: [Explorer.Repo.PrometheusLogger],
   pool_size: 20,
   pool_timeout: 60_000,
